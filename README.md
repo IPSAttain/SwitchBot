3657551ee2c603b63a37aa155c329c3d86e1fe98
IPSAttain <35669489+IPSAttain@users.noreply.github.com>
2022-12-24 23:25:41 +0100

    New Devices (#5)
    * Curtain
    * Lock
    * Plug
    * Strip Light
    * Color Bulb
    
    * Add Documentation

--- README.md
+++ README.md
@@ -1,6 +1,60 @@
-# Switchbot
+# Symcon Module SwitchBot
+### API Version 1.1
 
-# SwitchBot API v1.0
+### Inhaltsverzeichnis
+
+1. [Funktionsumfang](#1-funktionsumfang)
+2. [Voraussetzungen](#2-voraussetzungen)
+3. [Software-Installation](#3-software-installation)
+4. [Einrichten der Instanzen in IP-Symcon](#4-einrichten-der-instanzen-in-ip-symcon)
+5. [Entwickler Keys](#5-entwickler-keys)
+6. [Instanz Dokumentation](#6-instanz-dokumentation)
+7. [Weitere Links](#7-weitere-links)
+
+8. [Spenden](#8-spenden)
+
+### 1. Funktionsumfang
+
+
+### Das Modul unterstützt aktuell (24.12.2022) folgende Geräte.
+* SwitchBot Bot (Cloud Unterstützung muss aktiviert sein)
+* IR Fernbedienung - Light
+
+### 2. Voraussetzungen
+
+* IP-Symcon ab Version 6.0
+* SwitchBot Hub Mini
+* SwitchBot App (IOS oder Android) 
+* Registrierten SwitchBot Account
+
+
+### 3. Software-Installation
+
+* Über den Module Store das 'SwitchBot Splitter'-Modul installieren.
+* Alternativ über das Module Control folgende URL hinzufügen
+
+### 4. Einrichten der Instanzen in IP-Symcon
+
+* SwitchBot Konfigurator installieren
+* Es wird automatisch eine Splitter Instanz hinzugefügt.
+* Im Splitter müssen  ein Token Key und ein Security Key eingegeben werden. Diesen findet Ihr in der SwitchBot App. 
+* Weitere Infos im nächsten Kapitel
+* Weitere Informationen zum Hinzufügen von Instanzen in der [Dokumentation der Instanzen](https://www.symcon.de/service/dokumentation/konzepte/instanzen/#Instanz_hinzufügen)
+
+### 5. Entwickler Keys
+
+Den nötigen Token Key und den Security Key müssen in der App generiert werden.
+* Öffne App
+* Gehe zum Profil
+* Einstellungen
+* Tippe 10 mal auf das App Versions Feld
+* Damit werden die Entwikler Optionen aktiviert und ein neuer Menu Eintrag wird angezeigt.
+* Gehe zum Menu Entwickler Optionen
+* Generiere einen neuen Key
+![Keys](libs\SwitchBot1.jpg)
+
+
+### 6. Instanz Dokumentation
 
 Folgende Module beinhaltet das Switchbot Repository:
 
@@ -12,3 +66,14 @@ Folgende Module beinhaltet das Switchbot Repository:
 
 - __SwitchBot Device__ ([Dokumentation](SwitchBot%20Device))  
 	Kurze Beschreibung des Moduls.
+
+### 7. Weitere Links
+
+* [SwitchBot (Official website)](https://www.switch-bot.com/)
+* [API Dokumentation (GitHub)](https://github.com/OpenWonderLabs/SwitchBotAPI)
+
+### 8. Spenden
+
+Schenkungen zur Unterstützung sind hier möglich:
+
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6CSLZLEKKGT8Q" target="_blank"><img src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donate_LG.gif" border="0" /></a>
--- SwitchBot Device/module.php
+++ SwitchBot Device/module.php
@@ -31,14 +31,51 @@ declare(strict_types=1);
                 case 'Plug':
                     $this->RegisterVariableBoolean('setState', $this->Translate('Press'), '~Switch', 20);
                     $this->EnableAction('setState');
+                    $this->RegisterProfile('SwitchBot.toggle', 'TurnLeft', '', '', 0, 0, 0, '', 1);
+                    IPS_SetVariableProfileAssociation('SwitchBot.toggle', 0, $this->Translate('Toggle'), 'TurnLeft', 0xFFFFFF);
                     break;
                     
+                case 'Lock':
+                    $this->RegisterVariableBoolean('setLock', $this->Translate('Lock'), '~Lock', 20);
+                    $this->EnableAction('setLock');
+                    break;
+                
+                case 'Curtain':
+                    $this->RegisterVariableBoolean('setState', $this->Translate('Curtain'), '~ShutterMove', 20);
+                    $this->EnableAction('setState');
+                    $this->RegisterVariableInteger('setShutterPosition', $this->Translate('Curtain'),'~ShutterPosition.100', 21);
+                    $this->EnableAction('setShutterPosition');
+                    break;
+
                 case 'Light':
-                        $this->RegisterVariableBoolean('setState', $this->Translate('Press'), '~Switch', 20);
-                        $this->EnableAction('setState');
-                        $this->RegisterVariableBoolean('setBrightness', $this->Translate('Brightness'), 'Brightness', 20);
-                        $this->EnableAction('setBrightness');
-                        break;
+                    $this->RegisterVariableBoolean('setState', $this->Translate('Press'), '~Switch', 20);
+                    $this->EnableAction('setState');
+
+                    $this->RegisterProfile('SwitchBot.setBrightnessUp', 'HollowArrowUp', '', '', 0, 0, 0, '' , 1);
+                    IPS_SetVariableProfileAssociation('SwitchBot.setBrightnessUp', 0, $this->Translate('Brightness Up'), 'HollowArrowUp', 0xFFFFFF); 
+                    $this->RegisterVariableInteger('brightnessUp', $this->Translate('Brightness Up'), 'SwitchBot.setBrightnessUp', 31);
+                    $this->EnableAction('brightnessUp');
+
+                    $this->RegisterProfile('SwitchBot.setBrightnessDown', 'HollowArrowDown', '', '', 0, 0, 0, '', 1);
+                    IPS_SetVariableProfileAssociation('SwitchBot.setBrightnessDown', 0, $this->Translate('Brightness Down'), 'HollowArrowDown', 0xFFFFFF);
+                    $this->RegisterVariableInteger('brightnessDown', $this->Translate('Brightness Down'), 'SwitchBot.setBrightnessDown', 32);
+                    $this->EnableAction('brightnessDown');
+                    break;
+                
+                case 'Color Bulb':
+                case 'Strip Light':
+                    $this->RegisterVariableBoolean('setState', $this->Translate('Light'), '~Switch', 20);
+                    $this->EnableAction('setState');
+                    $this->RegisterVariableInteger('setBrightness', $this->Translate('Brightness'), '~Intensity.100', 31);
+                    $this->EnableAction('setBrightness');
+                    $this->RegisterVariableInteger('setColor', $this->Translate('Color'), '~HexColor', 32);
+                    $this->EnableAction('setColor');
+                    $this->RegisterVariableInteger('setColorTemperature', $this->Translate('Color Temperature'), '~TWColor', 33);
+                    $this->EnableAction('setColorTemperature');
+                    $this->RegisterProfile('SwitchBot.toggle', 'TurnLeft', '', '', 0, 0, 0, '', 1);
+                    IPS_SetVariableProfileAssociation('SwitchBot.toggle', 0, $this->Translate('Toggle'), 'TurnLeft', 0xFFFFFF);
+
+                    break;
             }
         }
 
@@ -51,7 +88,9 @@ declare(strict_types=1);
                 case 'Light':
                     $form = json_decode(file_get_contents(__DIR__ . '/../libs/formLightIRDevice.json'), true);
                     break;
-
+                case 'Curtain':
+                    $form = json_decode(file_get_contents(__DIR__ . '/../libs/formCurtainDevice.json'), true);
+                    break;
                 default:
                 $form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
             }
@@ -70,50 +109,55 @@ declare(strict_types=1);
             $data = array('deviceID' => $this->ReadPropertyString('deviceID'), 'parameter' => 'default', 'commandType' => 'command');
             switch ($Ident) {
                 case 'setState':
-                    $switchMode = $this->ReadPropertyBoolean('deviceMode');
-                    if (!$switchMode) {
-                        $data['command'] = 'press';
-                    } else {
-                        if ($Value) {
-                            $data['command'] = 'turnOn';
-                        } else {
-                            $data['command'] = 'turnOff';
-                        }
-                    }
-                    $this->SendDebug(__FUNCTION__, $data['command'], 0);
-                    $return = $this->SendData($data = json_encode($data));
-                    $return = json_decode($return, true);
-                    if (isset($return['body']['items'][0]['status']['battery'])) {
-                        $this->RegisterVariableInteger('battery', $this->Translate('Battery'), '~Battery.100', 30);
-                        $this->SetValue('battery',$return['body']['items'][0]['status']['battery']);
-                        $this->SendDebug(__FUNCTION__, 'Battery: ' . $return['body']['items'][0]['status']['battery'], 0);
-                    }
-                    $success = $return['message'];
-                    if ($success == 'success') {
-                        $this->SetValue($Ident, $Value);
-                        if (!$switchMode) {
-                            IPS_Sleep(2000);
-                            $this->SetValue($Ident, false);
-                        }
-                    }
+                    $data['command'] = 'turnOff';
+                    if ($Value) $data['command'] = 'turnOn';
+                    break;
+
+                case 'setColor':
+                    $data['command'] = $Ident;
+                    $data['parameter'] = strval($Value >> 16 & 255) . ':' . strval($Value >> 8 & 255) . ':' . $Value & 255;
                     break;
 
                 case 'setBrightness':
-                    if ($Value) {
-                        $data['command'] = 'brightnessUp';
-                    } else {
-                        $data['command'] = 'brightnessDown';
-                    }
-                    $this->SendDebug(__FUNCTION__, $data['command'], 0);
-                    $return = $this->SendData($data = json_encode($data));
-                    $return = json_decode($return, true);
-                    if ($return['message'] == 'success') $this->SetValue($Ident, $Value);
+                case 'setColorTemperature':
+                    $data['command'] = $Ident;
+                    $data['parameter'] = strval($Value);
+                    break;
+                
+                case 'brightnessUp':
+                case 'brightnessDown':
+                case 'toggle':
+                    $data['command'] = $Ident;
                     break;
 
+                case 'setLock':
+                    $data['command'] = 'unlock';
+                    if ($Value) $data['command'] = 'lock';
+                    break;
+                
+                case 'setShutterPosition':
+                    $data['command'] = 'setPosition';
+                    if ($this->ReadPropertyBoolean('deviceMode')) $data['parameter'] = '0,1,' . $Value;
+                    else $data['parameter'] = '0,0,' . $Value;
+
                 default:
-                    $this->SetValue($Ident, $Value);
+                    $data['command'] = 'unknown';
+            }
+            $this->SendDebug(__FUNCTION__, $data['command'], 0);
+            $return = json_decode($this->SendData($data = json_encode($data)), true); // Send Command to Splitter
+            if ($return['message'] == 'success' && $Ident == 'setState') {
+                $this->SetValue($Ident, $Value);
+                if ($this->ReadPropertyBoolean('deviceMode')) {
+                    IPS_Sleep(1000);
+                    $this->SetValue($Ident, false);
                 }
+            }
             $this->SendDebug(__FUNCTION__, 'ReturnMessage: ' . $return['message'], 0);
+            if (isset($return['body']['items'][0]['status']['battery'])) {
+                $this->RegisterVariableInteger('battery', $this->Translate('Battery'), '~Battery.100', 30);
+                $this->SetValue('battery',$return['body']['items'][0]['status']['battery']);
+                $this->SendDebug(__FUNCTION__, 'ReturnBatteryValue: ' . $return['body']['items'][0]['status']['battery'], 0);
+            }
             return $return;
         }
 
@@ -137,4 +181,21 @@ declare(strict_types=1);
             $this->SendDebug('Answer from API', $return, 0);
             return $return;
         }
+
+        protected function RegisterProfile($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Vartype)
+        {
+            if (!IPS_VariableProfileExists($Name)) {
+                IPS_CreateVariableProfile($Name, $Vartype); // 0 boolean, 1 int, 2 float, 3 string,
+            } else {
+                $profile = IPS_GetVariableProfile($Name);
+                if ($profile['ProfileType'] != $Vartype) {
+                    $this->SendDebug(__FUNCTION__, 'Variable profile type does not match for profile ' . $Name, 0);
+                }
+            }
+            IPS_SetVariableProfileIcon($Name, $Icon);
+            IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+            if ($Digits != '') IPS_SetVariableProfileDigits($Name, $Digits); //  Nachkommastellen
+            IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize); // string $ProfilName, float $Minimalwert, float $Maximalwert, float $Schrittweite
+        }
     }
+
