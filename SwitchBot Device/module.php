5380d2673a20f4d6a6c947f0a924c1d476e5726a
IPSAttain <35669489+IPSAttain@users.noreply.github.com>
2022-12-28 14:48:32 +0100

    Update module.php

--- SwitchBot Device/module.php
+++ SwitchBot Device/module.php
@@ -119,7 +119,6 @@ declare(strict_types=1);
 
         public function GetConfigurationForm()
         {
-            $values = json_decode($this->GetFormData());
             $this->SendDebug("Elements", json_encode($Values), 0);
             $form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
             switch ($this->ReadPropertyString('deviceType')) {
